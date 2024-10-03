d5316dc169c07c021ee49823ca0c7a7785746b14
juancarlossequeira <juan.sequeirajimenez@ucr.ac.cr>
2024-10-03 00:51:50 -0600

    Agregar cambios

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function sumarDos() {
     // Obtener el valor del input
     var input = document.getElementById('numberInput').value;
+    const errorMessage = document.getElementById("errorMessage");
     // Convertir el valor a un número
     var numero = parseFloat(input);
+
+    // Verifica si el número es negativo o mayor a 1000
+    if (input < 0 || input > 100000001) {
+        errorMessage.textContent = 'Por favor, introduce un número entre 0 y 100000000.'; // Mensaje de error
+        return; // Detiene la ejecución de la función
+    }
+
+    errorMessage.textContent = '';
 
     var cantidadOro = 0;
     var cantidadDiamante = 0;
@@ -39,6 +48,59 @@ function sumarDos() {
     document.getElementById('clasicaOro').innerHTML = clasicaOro;
     document.getElementById('clasicaDiam').innerHTML = clasicaDiam;
     document.getElementById('clasicaRubi').innerHTML = clasicaRubi;
+
+    document.getElementById('cantidadOro').innerHTML = cantidadOro;
+    document.getElementById('cantidadDiam').innerHTML = cantidadDiamante;
+    document.getElementById('cantidadRubi').innerHTML = cantidadRubi;
+
+    document.getElementById('probaOro').innerHTML = probaOro;
+    document.getElementById('probaDiam').innerHTML = probaDiam;
+    document.getElementById('probaRubi').innerHTML = probaRubi;
+
+}
+
+function sumarCuatro() {
+    // Obtener el valor del input
+    var input = document.getElementById('numberInput').value;
+    const errorMessage = document.getElementById("errorMessage");
+    // Convertir el valor a un número
+    var numero = parseFloat(input);
+
+    // Verifica si el número es negativo o mayor a 1000
+    if (input < 0 || input > 100000001) {
+        errorMessage.textContent = 'Por favor, introduce un número entre 0 y 100000000.'; // Mensaje de error
+        return; // Detiene la ejecución de la función
+    }
+
+    errorMessage.textContent = '';
+
+    var cantidadOro = 0;
+    var cantidadDiamante = 0;
+    var cantidadRubi = 0;
+
+    for (var i = 0; i < numero; i++) {
+        var numeroAleatorio = Math.random();
+        if (0 <= numeroAleatorio && numeroAleatorio < 0.2) {
+            cantidadDiamante = cantidadDiamante + 1;
+        } if (0.2 <= numeroAleatorio && numeroAleatorio < 0.5) {
+            cantidadRubi = cantidadRubi + 1;
+        } if (0.5 <= numeroAleatorio && numeroAleatorio <= 1) {
+            cantidadOro = cantidadOro + 1;
+        }
+    }
+
+    var probaOro = cantidadOro / numero;
+    var probaDiam = cantidadDiamante / numero;
+    var probaRubi = cantidadRubi / numero;
+
+    var esperadaOro = numero / 2;
+    var esperadoDiam = numero / 5;
+    var esperadoRubi = numero * 3 / 10;
+
+    var clasicaOro = 1/2;
+    var clasicaDiam = 3/10;
+    var clasicaRubi = 1/5;
+
 
     document.getElementById('cantidadOro').innerHTML = cantidadOro;
     document.getElementById('cantidadDiam').innerHTML = cantidadDiamante;
